7c2efc45cf7f80fa31114926df59a6c6c8162634
Katarina <klingat@me.com>
2017-07-21 20:50:35 +0000

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  before_save { self.email = email.downcase }
+  
+  # name validation
+  validates :name, 
+            presence: true,
+            length: { maximum: 50 }
+            
+  # email validation
+  validates :email, 
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+            
+  has_secure_password
+  
+end
